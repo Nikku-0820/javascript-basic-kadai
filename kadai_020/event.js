0e3ba7e6829793fe949dd39ac04ff538bd63242e
Nikku-0820 <163307882+Nikku-0820@users.noreply.github.com>
2024-05-28 18:36:28 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,8 @@
 const btn =document.getElementById('btn');
 
+//HTML要素がクリックされた時にイベント処理を実行する
 btn.addEventListener('click',() => {
-  console.log('ボタンをクリックしました');
+    console.log('ボタンをクリックしました');
+    text.textContent = 'ボタンをクリックしました';
 });
+
